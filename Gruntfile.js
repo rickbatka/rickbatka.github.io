2e43c3af0c2f32ab73eee7846e824ea579068bfd
Rick Batka <rick@Ricks-MacBook-Pro.local>
2015-10-04 19:08:14 -0700

    font testing

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,9 +17,30 @@ module.exports = function(grunt) {
 				files: 'Sass/*.scss',
 				tasks: ['sass']
 			}
-		}
+		},
+		'http-server': {
+        'dev': {
+            //root: '.',
+            port: 8080,
+            host: "0.0.0.0",
+            //cache: <sec>,
+            showDir : true,
+            autoIndex: true,
+            // server default file extension
+            ext: "html",
+            // run in parallel with other tasks
+            runInBackground: true
+            // specify a logger function. By default the requests are
+            // sent to stdout.
+            //logFn: function(req, res, error) { },
+        }
+    }
 	});
 	grunt.loadNpmTasks('grunt-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.registerTask('default',['watch']);
+	grunt.loadNpmTasks('grunt-http-server');
+	grunt.registerTask('default',[
+		'http-server',
+		'watch'
+	]);
 }
