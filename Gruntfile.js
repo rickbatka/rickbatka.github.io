0811039e5f78882b382ae42ea3b5808c8def46b7
Rick Batka <rick@Ricks-MacBook-Pro.local>
2015-10-04 21:45:34 -0700

    starting to build out resume

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,19 +20,14 @@ module.exports = function(grunt) {
 		},
 		'http-server': {
         'dev': {
-            //root: '.',
             port: 8080,
-            host: "0.0.0.0",
-            //cache: <sec>,
+						host: '0.0.0.0',
             showDir : true,
             autoIndex: true,
             // server default file extension
             ext: "html",
             // run in parallel with other tasks
             runInBackground: true
-            // specify a logger function. By default the requests are
-            // sent to stdout.
-            //logFn: function(req, res, error) { },
         }
     }
 	});
