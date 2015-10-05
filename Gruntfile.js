23df228ec13fc3c4614944f0b0186ba8a88d91e9
Rick Batka <rick@Ricks-MacBook-Pro.local>
2015-10-04 17:20:12 -0700

    setting up grunt and sass...

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		sass: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'Sass',
+          src: ['*.scss'],
+          dest: '../Css',
+          ext: '.css'
+        }]
+      }
+		},
+		watch: {
+			css: {
+				files: 'Sass/*.scss',
+				tasks: ['sass']
+			}
+		}
+	});
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.registerTask('default',['watch']);
+}
