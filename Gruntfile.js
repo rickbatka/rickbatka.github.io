0adb2056c152c8461ec3d7b4121dc6e4688625e7
Rick Batka <rick@Ricks-MacBook-Pro.local>
2015-12-30 11:46:28 -0500

    formatting json

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,15 +2,15 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		sass: {
-      dist: {
-        files: [{
-          expand: true,
-          cwd: 'Sass',
-          src: ['*.scss'],
-          dest: 'Css',
-          ext: '.css'
-        }]
-      }
+			dist: {
+				files: [{
+					expand: true,
+					cwd: 'Sass',
+					src: ['*.scss'],
+					dest: 'Css',
+					ext: '.css'
+				}]
+			}
 		},
 		watch: {
 			css: {
@@ -19,17 +19,17 @@ module.exports = function(grunt) {
 			}
 		},
 		'http-server': {
-        'dev': {
-            port: 8080,
-						host: '0.0.0.0',
-            showDir : true,
-            autoIndex: true,
-            // server default file extension
-            ext: "html",
-            // run in parallel with other tasks
-            runInBackground: true
-        }
-    }
+			'dev': {
+				port: 8080,
+				host: '0.0.0.0',
+				showDir : true,
+				autoIndex: true,
+				// server default file extension
+				ext: "html",
+				// run in parallel with other tasks
+				runInBackground: true
+			}
+		}
 	});
 	grunt.loadNpmTasks('grunt-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
